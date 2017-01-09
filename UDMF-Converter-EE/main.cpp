005bd6b9448bc1ed464b23c300a3ca2f605f56db
Ioan Chera <ioan.chera@gmail.com>
2017-01-09 23:19:06 +0200

    Initial Commit

---
+++ UDMF-Converter-EE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UDMF-Converter-EE
+//
+//  Created by ioan on 09.01.2017.
+//  Copyright © 2017 Team Eternity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
